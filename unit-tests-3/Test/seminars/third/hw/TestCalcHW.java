994b6ac24228a65fd321b9651dac3accebb4c219
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-10-17 18:22:34 +0500

    ++

---
+++ unit-tests-3/Test/seminars/third/hw/TestCalcHW.java
@@ -0,0 +1,26 @@
+package seminars.third.hw;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seminars.third.hw.CalcHW.evenOddNumber;
+import static seminars.third.hw.CalcHW.numberInInterval;
+
+public class TestCalcHW {
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {-3,-2,-1,0,1,2,3})
+    public void testEvenOddNumber(int i) {
+        assertEquals(evenOddNumber(i), ((i % 2) == 0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, 0, 10, 24, 25, 50, 99, 100, 120})
+    public void testNumberInInterval(int i) {
+        assertEquals(numberInInterval(i),(25<i && i<100) );
+    }
+
+
+}
